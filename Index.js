edaedd369c74f88f80dc78b83740d34bbee2355c
ayushkumar8877 <ayushkumar88770@gmail.com>
2026-01-11 15:20:04 +0530

    first commit

--- Index.js
+++ Index.js
@@ -15,11 +15,11 @@ app.use(cookieParser());
 
 app.use('/api/v1/signup',express.static(path.join(__dirname,'public/Signup.html')));
 app.use('/api/v1/login',express.static(path.join(__dirname,'public/login.html')));
-app.use('/api/v1/AdminHome',express.static(path.join(__dirname,'public/AdminHome.html')));
+// app.use('/api/v1/AdminHome',express.static(path.join(__dirname,'public/AdminHome.html')));
 //app.use('/api/v1/sendotp',express.static(path.join(__dirname,'public/otp.html')));
 app.use('/api/v1/addProfile',express.static(path.join(__dirname,'public/Regis.html')));
-app.use('/api/v1/Stateinfo',express.static(path.join(__dirname,'public/addexper.html')));
-app.use('/api/v1/addCenter',express.static(path.join(__dirname,'public/center.html')));
+// app.use('/api/v1/Stateinfo',express.static(path.join(__dirname,'public/addexper.html')));
+// app.use('/api/v1/addCenter',express.static(path.join(__dirname,'public/center.html')));
 
 
 require("dotenv").config();
--- public/sendotp.js
+++ public/sendotp.js
@@ -1,15 +1,15 @@
-function sendOTP() {
-    const email = $('#email').val();
+          function sendOTP() {  
+    const mobile = $('#mobile').val();
   
     $.ajax({
       url: '/api/v1/sendotp',
       type: 'POST',
-      data: { email: email },
+      data: { mobile: mobile},
       success: function(response) {
         // Update the page based on the response
         if (response.success) {
           // Show a message that the OTP has been sent
-          alert('OTP sent to email');
+          alert('OTP sent to');
         } else {
           // Show an error message
           alert('Error sending OTP');
